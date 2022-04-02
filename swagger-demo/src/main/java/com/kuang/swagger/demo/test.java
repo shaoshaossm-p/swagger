69e45bbeea1a6b24c3d2860a5074b47e15e51b95
shaoshaossm-p <102852789+shaoshaossm-p@users.noreply.github.com>
2022-04-02 17:57:34 +0800

    Update test.java

--- swagger-demo/src/main/java/com/kuang/swagger/demo/test.java
+++ swagger-demo/src/main/java/com/kuang/swagger/demo/test.java
@@ -9,6 +9,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
+            System.out.println("t再次测试");
         System.out.println("test2");
     }
 }
